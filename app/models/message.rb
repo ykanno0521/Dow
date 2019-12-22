376bad55406f7eae25677e4384162ba834dceee3
ykanno0521 <kanno0521@gmail.com>
2019-12-22 12:05:39 +0900

    Add:メッセージ文字数省略

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,9 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user, optional: true
   belongs_to :walker, optional: true
+
+  # def short_description
+  #   description[0, 20] + '...'
+  # end
+
 end
