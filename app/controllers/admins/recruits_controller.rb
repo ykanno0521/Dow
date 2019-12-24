dd70bd6acdcdaa23c66ac9ed9ac61b0a9eecd4bb
ykanno0521 <kanno0521@gmail.com>
2019-12-24 20:26:44 +0900

    Add:admin側recruit画面追加

--- app/controllers/admins/recruits_controller.rb
+++ app/controllers/admins/recruits_controller.rb
@@ -1,8 +1,10 @@
 class Admins::RecruitsController < ApplicationController
   def index
+    @recruits = Recruit.all
   end
 
   def show
+    @recruit = Recruit.find(params[:id])
   end
 
   def destroy
